Pedantic scaling fix

sim_0.py
@@ -67,7 +67,7 @@ y_a = L1*np.sin(np.radians(th_1))
 # plotting code (adapted)
 
 fig = plt.figure(figsize=(5, 4))
-ax = fig.add_subplot(autoscale_on=False, xlim=(-1.5, (L1+L2+0.5)), ylim=(-(L1+L2), (L1+L2)))
+ax = fig.add_subplot(autoscale_on=True, xlim=(-0.01, (L1+L2+0.01)), ylim=(-(L1+L2), (L1+L2)))
 ax.set_aspect('equal')
 ax.grid()
 