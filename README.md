5736a425eff81bccfbfcb367287b35c59595c221
Sez26 <serena.farrelly@gmail.com>
2024-11-04 12:55:58 +0000

    initialisation SF

--- README.md
+++
@@ -1,1 +0,0 @@
-# Mulivariable Nonlinear Control Uni2024
--- arduino_control/Archive/dumb_circle/circle_gen.ino
+++
@@ -1,9 +0,0 @@
-#include <math.h>
-
-float tarx(float o_x, float r, float th0){
-  return o_x + (r * cos(th0));
-}
-
-float tary(float o_y, float r, float th0){
-  return o_y + (r * sin(th0));
-}
