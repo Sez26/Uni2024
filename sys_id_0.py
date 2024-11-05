f8b1dd8646b9c9f474b97e1ca21498e57425702b
sez26 <serena.farrelly@gmail.com>
2024-11-05 11:26:16 +0000

    adj sq 5 reference (originx 0.14)

--- sys_id_0.py
+++ sys_id_0.py
@@ -18,13 +18,13 @@ num_int = 1000
 r = 0.044
 sq_sl = 0.086
 tri_sl = 0.094
-origin = [0.12, 0]
+origin = [0.14, 0]
 L1 = 0.095
 L2 = 0.095
 
 # generate reference coordinates
-xy = ref_gen.circle_gen(r, origin, num_int)
-# [xy,num_int] = ref_gen.square_gen(sq_sl, origin, num_int)
+# xy = ref_gen.circle_gen(r, origin, num_int)
+[xy,num_int] = ref_gen.square_gen(sq_sl, origin, num_int)
 # [xy,num_int] = ref_gen.tri_gen(tri_sl, origin, num_int)
 
 
@@ -61,7 +61,7 @@ ref_t = np.linspace(0,drawtime, num_int)
 
 # combining arrays
 save_dir = '/home/sez26/Uni2024/MVNLC/Uni2024_MVNLC/reference_signals/'
-filename = 'ref_circ_9.h'
+filename = 'ref_sq_5.h'
 reference = np.column_stack((ref_t, th_1_w, th_2_w))
 
 enc_per_rot = 131.25*16
