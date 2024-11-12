1f0c88558e76c11ca1aedf0fc9c382f5591bb29e
sez26 <serena.farrelly@gmail.com>
2024-11-12 12:52:32 +0000

    45 degree square (originx 0.1)

--- sys_id_1.py
+++ sys_id_1.py
@@ -8,7 +8,7 @@ import math
 import numpy as np
 
 # external python code files
-import ref_gen_3 as ref_gen
+import ref_gen_4 as ref_gen
 import ref_print_0 as ref_print
 import hardware_conv_0  as hardware_conv
 import ref_fitting_2 as ref_fit
@@ -20,13 +20,14 @@ num_int = 1000
 r = 0.044
 sq_sl = 0.086
 tri_sl = 0.094
-origin = [0.14, 0]
+origin = [0.1, 0]
 L1 = 0.095
 L2 = 0.095
 
 # generate reference coordinates
 # xy = ref_gen.circle_gen(r, origin, num_int)
-[xy,num_int] = ref_gen.square_gen(sq_sl, origin, num_int)
+# [xy,num_int] = ref_gen.square_gen(sq_sl, origin, num_int)
+[xy,num_int] = ref_gen.sq_gen_45(sq_sl, origin, num_int)
 # [xy,num_int] = ref_gen.tri_gen(tri_sl, origin, num_int)
 
 
@@ -63,7 +64,7 @@ ref_t = np.linspace(0,drawtime, num_int)
 
 # combining arrays
 save_dir = '/home/sez26/Uni2024/MVNLC/Uni2024_MVNLC/reference_signals/'
-filename = 'ref_sq_9.h'
+filename = 'ref_sq_10.h'
 reference = np.column_stack((ref_t, th_1_w, th_2_w))
 
 enc_per_rot = 131.25*16
@@ -79,5 +80,5 @@ ref_new = ref_fit.b_spline_t(ref_new, 30, dt)
 ref_sig_plot.ref_plot([reference, ref_new], ["base", "b spline"])
 
 # calling print function
-ref_new = hardware_conv.Lizzy_adj(ref_new, 3)
+ref_new = hardware_conv.Lizzy_adj(ref_new, 4)
 ref_print.print_ref(save_dir,filename, ref_new)
