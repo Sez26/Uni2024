09264a7e3059f060361de5d37ba2434abc7a6b01
sez26 <serena.farrelly@gmail.com>
2024-11-14 10:57:22 +0000

    breakthrough with np flattening

--- ref_fitting_2.py
+++ ref_fitting_2.py
@@ -20,7 +20,8 @@ import sys
 from scipy.interpolate import make_interp_spline
 import matplotlib.pyplot as plt
 
-def S_curve(ref,max_a,dt): #  this can't be done on seperate thetas, because then the thetas would be shifted from eachother
+def S_curve(reference,max_a,dt): #  this can't be done on seperate thetas, because then the thetas would be shifted from eachother
+    ref = np.copy(reference)
     # find shape of ref (assume first column is for time series)
     (num_int,cols) = np.shape(ref)
     # number of theta signals (col-1)
@@ -33,66 +34,89 @@ def S_curve(ref,max_a,dt): #  this can't be done on seperate thetas, because the
 
     # get index values where acceleration is greater than max_a
     idx = np.abs(o_dot)>max_a
-    print(np.shape(idx))
+    # print(np.shape(idx))
 
     # find the first and last index values for each period of saturation
     diff_idx = np.diff(idx, axis=0) # when it != 0 then there is a change of state
     # for some reason this is squishing the arrays
 
-    print(np.shape(diff_idx))
+    # print(np.shape(diff_idx))
     brk_idx = np.where(diff_idx>0)
-    print(brk_idx)
+    # print(brk_idx)
     brk_row_idx = brk_idx[0]
     brk_col_idx = brk_idx[1]
-    # check if even (start and end pairs)
-    if len(brk_row_idx)%2 == 0:
-        pass
-    elif len(brk_row_idx)%2 != 0:
-        # check whether it is tail/start
-        if idx[0]: # if start acceleration is saturated
-            brk_idx = np.append(0,brk_idx)
-        elif idx[-1]: # if end acceleration is saturated
-            brk_idx = np.append(brk_idx, len(ref))
-    elif len(brk_idx) == 0:
-        print("Accelerations do not exceed programmed maximum. Reference is unchanged")
-        return ref
 
     # find start and end pair for each th signal
     th_sig_brk = []
     for i in range(cols-1):
         th_sig_brk.append(brk_row_idx[brk_col_idx==i])
+    # print(th_sig_brk)
+    # print(len(th_sig_brk[0])/2)
+    for i in range(cols-1):
+        # check if even (start and end pairs)
+        if len(th_sig_brk[i])%2 == 0:
+            pass
+        elif len(th_sig_brk[i])%2 != 0:
+            # check whether it is tail/start
+            if idx[0,i]: # if start acceleration is saturated
+                th_sig_brk[i] = np.append(0,th_sig_brk[i])
+            elif idx[-1,i]: # if end acceleration is saturated
+                th_sig_brk[i] = np.append(th_sig_brk[i], len(ref))
+        elif len(brk_idx) == 0:
+            print("Accelerations do not exceed programmed maximum. Reference is unchanged")
+            return ref
+    # print(th_sig_brk)
+    # print(len(th_sig_brk[0])/2)
+    # calculate stretch factor (more like the time required for max_a during saturation)
+    num_t = np.array((cols-1, 1), dtype=object)
+    for i in range(cols-1):
+        num_pair = int(len(th_sig_brk[i])/2)
+        num_t[i] = np.zeros((num_pair,1))
+        for j in range(int(len(num_t[i]))):
+            # num_t[i][j] = int(np.floor(np.sqrt((2*(np.abs(th_sig_brk[i][2*j+1]- th_sig_brk[i][2*j])/max_a)))))
+            num_t[i][j] = np.floor(np.sqrt((2*(np.abs(np.diff(th_sig_brk[i])[2*j]/max_a)))))
+       
+    print(num_t)
 
-    print(th_sig_brk)
+    # create substitute arrays
+    # Initialize rep_arr as a 1D object array for nesting
+    rep_arr = np.empty(cols-1, dtype=object)
 
-    # # assign maximum acceleration values to all point
-    # sat_acc = np.clip(o_dot, -max_a, max_a)   
-    # print(np.shape(sat_acc)) 
+    for i in range(cols-1):
+        num_pair = int(len(th_sig_brk[i]) / 2)
+        print("num_pair:", num_pair)
 
-    # # integrate back
-    # sat_o = np.zeros_like(om)
-    # sat_o[0,:] = om[0,:]
-    # sat_th = np.zeros_like(th)
-    # sat_th[0,:] = th[0,:]
-    # for i in range(1, len(sat_acc)):
-    #     for j in range(cols-1):
-    #         sat_o[i,j] = sat_o[i-1,j] + sat_acc[i-1,j] * dt
-    # for i in range(1, len(sat_o)):
-    #     for j in range(cols-1):
-    #         sat_th[i,j] = sat_th[i-1,j] + sat_o[i-1,j] * dt
-    
+        # Initialize rep_arr[i] to hold arrays of shape (num_pair, 1)
+        rep_arr[i] = np.empty(num_pair, dtype=object)
 
+        for j in range(num_pair):
+            print("num_t[i][j][0]:", num_t[i][j][0])
 
-    # # split theta values at break points
-    # spl_th = np.split(th,brk_row_idx)
-    # print(len(spl_th))
+            # Initialize the inner array with zeros
+            # print(rep_arr[i][j])
 
-    # # replace arrays
-    # # need to make this more robust
-    # S_th = np.column_stack((spl_th[0], rep_arrs[0], spl_th[2], rep_arrs[1]))
+            # this zeros array has to be for all thetas
+            rep_arr[i][j] = np.zeros((int(num_t[i][j][0]), cols-1), dtype=float)
+            print("rep_arr[i][j] shape: ", np.shape(rep_arr[i][j]))
+            for k in range(cols-1):
+            # Get index range and fill values based on th_sig_brk and th
+                # temp_arr = rep_arr[i][j].copy()
+                idx_ra = np.linspace(0, num_t[i][j][0]-1, np.diff(th_sig_brk[i])[2*j], dtype=int)
+                fill = th[th_sig_brk[i][2*j]:th_sig_brk[i][2*j+1], k].reshape(-1, 1)
+                # Place values from 'fill' into 'rep_arr' using idx_ra indices
+                # this is changing for each loop
+                rep_arr[i][j][idx_ra, k] = fill.flatten()
+                print(np.shape(rep_arr[i][j]))
+                # Interpolate to fill in gaps
+                rep_arr[i][j][:,k] = np.interp(
+                    np.arange(rep_arr[i][j][:,k].shape[0]),  # Full range
+                    idx_ra.flatten(),                   # Known index locations
+                    fill.flatten()                      # Known values
+                ) # Reshape to keep it 2D (num_t[i][j][0], 1)
+                print(np.shape(rep_arr[i][j]))
 
-    # for i in range(len(start_idx)):
+    print("Final rep_arr:", rep_arr[0][0][:,1])
 
-    return sat_th
 
 
 def b_spline_t(reference, num_smpl, dt):
@@ -100,12 +124,6 @@ def b_spline_t(reference, num_smpl, dt):
     # find shape of ref (assume first column is for time series)
     (num_int,cols) = np.shape(ref)
     # number of theta signals (col-1)
-
-    # find velocity and acceleration
-    # om = np.diff(ref, axis=0) #  len(th) -1
-    # om[:,0] = ref[:-1, 0]
-    # print(np.shape(om))
-    # o_dot = np.diff(om, axis=0) # len(th) -2
 
     # sample base velocity signal at num_smpl
     smpl_ref = ref[::(int(np.floor(num_int/num_smpl)))]
