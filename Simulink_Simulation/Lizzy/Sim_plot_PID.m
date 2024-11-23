212264361c76a642fad59f481072b4bb0e9b5a9e
lizjwh <145970608+lizjwh@users.noreply.github.com>
2024-11-23 13:16:42 +0000

    graphs

--- Simulink_Simulation/Lizzy/Sim_plot_PID.m
+++ Simulink_Simulation/Lizzy/Sim_plot_PID.m
@@ -36,41 +36,18 @@ plot(xData_PD, yData_PD, 'g', 'LineWidth', 0.5);
 plot(xData_PID, yData_PID, 'k', 'LineWidth', 0.5);
 %h = plot(xData(1), yData(1), 'go', 'MarkerSize', 8, 'MarkerFaceColor', 'g'); % Initial point
 
-%Plot square
-squareSide = 86; % Side length in mm
-cornerPoint = [77, -43]; % Bottom-left corner of the square (adjust as needed)
-xSquare = cornerPoint(1) + [0, squareSide, squareSide, 0, 0];
-ySquare = cornerPoint(2) + [0, 0, squareSide, squareSide, 0];
-plot(xSquare, ySquare, 'r', 'LineWidth', 0.5);
+% Plot reference signal
+xData_ref = (L1*cos(deg2rad(refTh1)) + L2*cos(deg2rad(refTh2)))*1000;
+yData_ref = (L1*sin(deg2rad(refTh1)) + L2*sin(deg2rad(refTh2)))*1000;
+plot(xData_ref, yData_ref, 'r', 'LineWidth', 0.5);
+xlim([min(xData_ref)-50, max(xData_ref)+50]);
+ylim([min(yData_ref)-50, max(yData_ref)+50]);
 
-%Plot triangle
-% triangleSide = 94; % Side length in mm
-% startPoint = [50, -40]; % Bottom-left corner of the triangle (adjust as needed)
-% xTriangle = [0, triangleSide, triangleSide / 2, 0] + startPoint(1); % x-coordinates
-% yTriangle = [0, 0, sqrt(triangleSide^2 - (triangleSide / 2)^2), 0] + startPoint(2); % y-coordinates
-% plot(xTriangle, yTriangle, 'r', 'LineWidth', 0.5);
-
-%Plot circle
-% circleRadius = 44;
-% circleCenter = [100, -20];
-% theta = linspace(0, 2*pi, 100); % Angle values from 0 to 2*pi
-% xCircle = circleCenter(1) + circleRadius * cos(theta); % x-coordinates
-% yCircle = circleCenter(2) + circleRadius * sin(theta); % y-coordinates
-% plot(xCircle, yCircle, 'r', 'LineWidth', 0.5);
-
+%Graph settings
 legend('PD Control', 'PID Control','Target shape');
 xlabel('X (mm)');
 ylabel('Y (mm)');
 title('Simulated Response of Different Control Methods');
 grid on;
 axis equal;
-xlim([min(xData)-50, max(xData)+50]);
-ylim([min(yData)-50, max(yData)+50]);
 
-figure;
-% Plot reference signal
-xData_ref = (L1*cos(deg2rad(refTh1.th1(1,:))) + L2*cos(deg2rad(refTh2.th2(1,:))))*1000;
-yData_ref = (L1*sin(deg2rad(refTh1.th1(1,:))) + L2*sin(deg2rad(refTh2.th2(1,:))))*1000;
-plot(xData_ref, yData_ref, 'b', 'LineWidth', 0.5);
-xlim([min(xData)-50, max(xData)+50]);
-ylim([min(yData)-50, max(yData)+50]);
