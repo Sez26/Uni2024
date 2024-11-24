e0397953b831288706d46cd2a8e5e33cbf0e65d4
lizjwh <145970608+lizjwh@users.noreply.github.com>
2024-11-24 22:38:38 +0000

    Update IAE.m

--- Simulink_Simulation/Lizzy/IAE.m
+++ Simulink_Simulation/Lizzy/IAE.m
@@ -1,18 +1,19 @@
 
 
 % get e and t from timeseries data
-function [e_th1, e_th2] = get_Data_from(workspace_name, L1, L2)
+function [e_th1, e_th2, time] = get_Data_from(workspace_name, L1, L2)
     error1 = workspace_name.get('e_th1');
     error2 = workspace_name.get('e_th2');
-    error1_vals = error1.Values
-    error2_vals = error2.Values
+    error1_vals = error1.Values;
+    error2_vals = error2.Values;
     e_th1 = error1_vals.Data;
     e_th2 = error2_vals.Data;
+    time = error1_vals.Time; %verified both time series are the same
 end
 
-[e_th1, e_th2] = get_Data_from(Plant_0, L1, L2);
+[e_th1, e_th2, time] = get_Data_from(Plant_0, L1, L2);
 
-%IAE = trapz(t, abs(e)); % t is the time vector, e is the error vector
+IAE_th1 = trapz(time, abs(e_th1));
 
 % Plot IAE against time
-%plot(IAE_0, t,'k', 'LineWidth', 0.5)
+plot(IAE_th1, time,'k', 'LineWidth', 0.5)
