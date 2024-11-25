fc7d7d36b7c361df53aa2bc403cb7751e3e68a74
lizjwh <145970608+lizjwh@users.noreply.github.com>
2024-11-25 12:19:45 +0000

    IAE

--- Simulink_Simulation/Lizzy/IAE_distances.m
+++ Simulink_Simulation/Lizzy/IAE_distances.m
@@ -1,7 +1,7 @@
 % get e and t from timeseries data
 function [err, time] = get_Data_from(workspace_name, L1, L2)
-    theta1Signal = workspace_name.get('th1'); % Get 'theta1' signal
-    theta2Signal = workspace_name.get('th2'); % Get 'theta2' signal
+    theta1Signal = workspace_name.get('th1');
+    theta2Signal = workspace_name.get('th2');
     theta1Timeseries = theta1Signal.Values;
     theta2Timeseries = theta2Signal.Values;
     th1Data = theta1Timeseries.Data;
@@ -10,27 +10,26 @@ function [err, time] = get_Data_from(workspace_name, L1, L2)
     yData_th2 = (L1*sin(deg2rad(th1Data)) + L2*sin(deg2rad(th2Data)))*1000;
 
     %Import reference x y positions
-    theta1Signal = workspace_name.get('ref_th1'); % Get 'theta1' signal
-    theta2Signal = workspace_name.get('ref_th2'); % Get 'theta2' signal
+    theta1Signal = workspace_name.get('ref_th1'); 
+    theta2Signal = workspace_name.get('ref_th2');
     theta1Timeseries = theta1Signal.Values;
     theta2Timeseries = theta2Signal.Values;
     th1Data = theta1Timeseries.Data;
     th2Data = theta2Timeseries.Data;
     xData_ref1 = (L1*cos(deg2rad(th1Data)) + L2*cos(deg2rad(th2Data)))*1000;
     yData_ref2 = (L1*sin(deg2rad(th1Data)) + L2*sin(deg2rad(th2Data)))*1000;
-    
+
+    %Return time
+    time = theta1Timeseries.Time; %verified both time series are the same
+
     %Calculate error distances
     response = [xData_th1, yData_th2];
     ref = [xData_ref1, yData_ref2];
-    err = sqrt(sum((response - ref).^2, 2));
-    
-    %Time
-    time = theta1Timeseries.Time; %verified both time series are the same
+    err = sqrt(sum((response - ref).^2, 2));    
 end
 
 function plot_this(plant, L1, L2)
-    [err, time] = get_Data_from(plant, L1, L2);      %get errors from timeseries data
-    err = abs(deg2rad(err));   %turn errors into absolute values
+    [err, time] = get_Data_from(plant, L1, L2); %get errors from timeseries data
     
     if time(2)-time(1) == time(3)-time(2) %time steps are the same between each point
         dt = time(2) - time(1); % Time difference between consecutive points
@@ -44,13 +43,16 @@ function plot_this(plant, L1, L2)
 end
 
 % Plot IAE against time
+clear figure;
 figure; hold on;
-plot_this(Plant_0, L1, L2); %square
 
-% plot_this(Plant_1_circle);
+%plot_this(PD_control, L1, L2);
+plot_this(PID_control, L1, L2);
+plot_this(Plant_0, L1, L2);
 % plot_this(Plant_2_1_circle);
-% plot_this(Plant_3_circle);
+plot_this(Plant_3, L1, L2);
+
 xlabel('Time (s)');
-ylabel('IAE (rad.s)');
+ylabel('IAE (rad.s)'); 
 legend;
 title('Non linear model: Integral of Absolute Error')
