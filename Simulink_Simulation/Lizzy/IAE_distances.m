11b0e08125013e3c521c2dd995aa1e0fa77c951e
lizjwh <145970608+lizjwh@users.noreply.github.com>
2024-11-26 12:19:30 +0000

    backlash

--- Simulink_Simulation/Lizzy/IAE_distances.m
+++ Simulink_Simulation/Lizzy/IAE_distances.m
@@ -50,10 +50,11 @@ plot_this(Plant_0, L1, L2, false);
 plot_this(Plant_2_1, L1, L2,true);
 %plot_this(Plant_3, L1, L2, false);
 plot_this(PID_lowpass,L1,L2,false);
+plot_this(PID_antiwindup,L1,L2,false);
 
 xlabel('Time (s)');
 ylabel('IAE (rad.s)');
-legend('Linear Model', 'Nonlinear Model', 'State Feedback Control', 'Lowpass');
+legend('Linear Model', 'Nonlinear Model', 'State Feedback Control', 'Lowpass 1','Antiwindup');
 title('Integral of Absolute Error for a Square Reference')
 
 % figure;
