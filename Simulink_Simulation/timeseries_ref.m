2f673b42856da842c0001b0900af3f4ac7eb72a2
Sez26 <serena.farrelly@gmail.com>
2024-11-29 16:56:47 +0000

    new squares

--- Simulink_Simulation/timeseries_ref.m
+++ Simulink_Simulation/timeseries_ref.m
@@ -2,7 +2,7 @@
 % converting array into timesseries type
 addpath('ref_sig\')
 
-filename = 'ref_sq_bs_1.mat';
+filename = 'ref_sq_bs_2.mat';
 data = load(filename);
 time_arr = data.reference(:,1)';
 % ts3 = timeseries((1:5)',[0 10 20 30 40]);
@@ -13,7 +13,7 @@ th2 = [time_arr; data.reference(:,3)'];
 % dth1 = [time_arr; data.reference(:,4)'];
 % dth2 = [time_arr; data.reference(:,5)'];
 
-save(".\ref_source\20s\ref_sq_bs_th1_1.mat", "th1");
-save(".\ref_source\20s\ref_sq_bs_th2_1.mat", "th2");
+save(".\ref_source\20s\ref_sq_bs_th1_2.mat", "th1");
+save(".\ref_source\20s\ref_sq_bs_th2_2.mat", "th2");
 % save("ref_dth1.mat", "dth1");
 % save("ref_dth2.mat", "dth2");
