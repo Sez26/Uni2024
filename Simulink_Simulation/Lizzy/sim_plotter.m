9ce26e966d885163234dcf6ca0e1ad383587fdb1
lizjwh <145970608+lizjwh@users.noreply.github.com>
2024-12-01 21:03:41 +0000

    sync

--- Simulink_Simulation/Lizzy/sim_plotter.m
+++ Simulink_Simulation/Lizzy/sim_plotter.m
@@ -4,6 +4,8 @@ function plot_response(data, L1, L2, start, color)
     % Initial point
     % plot(xData_1(1), yData_1(1), 'o', 'MarkerSize', 5,'MarkerFaceColor', [0, 0.4470, 0.7410]); 
     plot(xData(start:end), yData(start:end),string(color),'LineWidth', 1.5);
+    disp(xData(start))
+    disp(yData(start))
 end
 
 function plot_reference(data, L1, L2, start, color)
@@ -32,18 +34,34 @@ function plot_image(image)
 
 end
 
+function plot_manual(color)
+    x_start = 42;
+    y_start = -137;
+    
+    % Define the side length of the square
+    side_length = 86;
+    
+    % Calculate the coordinates of the square's corners
+    x = [x_start, x_start + side_length, x_start + side_length, x_start, x_start];
+    y = [y_start, y_start, y_start + side_length, y_start + side_length, y_start];
+    plot(x, y, string(color), 'LineWidth',1.5);
+    xlim([min(x)-25, max(x)+25]);
+    ylim([min(y)-25, max(y)+25]);
+end
+
 % Create figure
 clear figure;
 figure('Renderer', 'painters', 'Position', [10 10 550 500]);
 hold on;
 
 start = 500; 
-plot_image('square1.jpg');
-plot_reference(PID_square_backlash, L1, L2, start, 'k'); 
-plot_response(PID_square_backlash, L1, L2, start, 'r');
+plot_image('triangle.jpg');
+%plot_manual('k');
+plot_reference(PID_triangle_backlash, L1, L2, start, 'k');
+plot_response(PID_triangle_backlash, L1, L2, start, 'r');
 
 %Graph settings ----------------------------
-legend('Target shape','No backlash','Backlash modelled'); %'Linear model','Nonlinear model','State feedback control','backlash','Target shape');
+legend('Real Response','Target shape','PID Model Response');
 xlabel('X (mm)');
 ylabel('Y (mm)');
 grid on; axis equal;
